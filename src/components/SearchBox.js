c3e37caa0d653fbdacdc5f70f94154d488886cc2
sun <sunpeiting07@gmail.com>
2022-02-19 18:16:54 +1030

    feat: memorize search key after refresh

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,15 +3,42 @@ import { useEffect, useState } from 'react'
 import SearchIcon from '@material-ui/icons/Search';
 
 export const SearchBox = ({onSearch}) => {
-  const [searchValue, setSearchValue] = useState("")
+  const [searchValue, setSearchValue] = useState("");
+  const [openSearch, setOpenSearch] = useState(false);
+
+  useEffect(() => {
+    //get local storage
+    if(window.localStorage.getItem('search'))
+    {
+      setSearchValue(window.localStorage.getItem('search'));
+      setOpenSearch(true)
+    } 
+    onSearch(searchValue)
+  },[])
+
+  useEffect ( () =>{
+    if(openSearch){
+      onSearch(searchValue);
+      storeInLocal(searchValue)
+      setOpenSearch(false)
+    }
+  },[openSearch])
+
+  /**
+   * storeInLocal: store search value in local storage
+   */
+  const storeInLocal = (searchValue) => {
+    window.localStorage.setItem('search', searchValue);
+  }
 
   return (
     <div className='search-box'>
       <input
         onChange={val => setSearchValue(val.currentTarget.value)}
         placeholder="Input Search"
+        value={searchValue}
       />
-      <button onClick={() => {onSearch(searchValue)}}>
+      <button onClick={() => {setOpenSearch(true); }}>
         <SearchIcon />
       </button>
     </div>
