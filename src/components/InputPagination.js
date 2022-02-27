63bbaba8de40957c9fe8a8a449bd2a609fa8ae90
sun <sunpeiting07@gmail.com>
2022-02-27 15:48:25 +1030

    fix: MA-21-clear warning message for invalid color value on Icon

--- src/components/InputPagination.js
+++ src/components/InputPagination.js
@@ -2,7 +2,7 @@ import '../utils/input_pagination.scss';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
-export const InputPagination = ({currentPage, totalResultNum, handleChangePage, movieCount}) => {
+export const InputPagination = ({currentPage, totalResultNum, handleChangePage}) => {
   const totalPage = Math.floor(totalResultNum / 10) + 1;
   const backPage = currentPage - 1;
   const forwardPage =  currentPage + 1;
@@ -16,8 +16,7 @@ export const InputPagination = ({currentPage, totalResultNum, handleChangePage, 
       </span>
       <span className='icon'>
         <ArrowBackIosIcon 
-          fontSize="small" 
-          color={currentPage == 1 ? 'disabled' : ''}
+          style={{color: currentPage == 1 ? 'rgba(255,255,255,0.4)' : '#fff'}}
           onClick={(evt) => {if(currentPage ==  1) return; handleChangePage(backPage)}}
         />
       </span>
@@ -28,12 +27,11 @@ export const InputPagination = ({currentPage, totalResultNum, handleChangePage, 
           if( evt.currentTarget.value > totalPage)
             return;
           handleChangePage(evt.currentTarget.value)
-        }}/>
+        }} />
         <span className='text'> / {totalPage}</span>
         <span className='icon'>
           <ArrowForwardIosIcon 
-            fontSize="small" 
-            color={currentPage ==  totalPage ? 'disabled' : ''} 
+            style={{color: currentPage == totalPage ? 'rgba(255,255,255,0.4)' : '#fff'}}
             onClick={(evt) => {if(currentPage ==  totalPage) return; handleChangePage(forwardPage)}}
           />
         </span>
--- src/components/TypeRadio.js
+++ src/components/TypeRadio.js
@@ -5,7 +5,7 @@ export const TypeRadio = ({title, handleOnChangeType,selectedType}) => {
   return (
     <label id="radio_con" className="radio-container"  onClick={(evt)=> {handleOnChangeType(title)}}>
       {selectedType == title ?
-        <input type="radio" name="radio" id="radio_icon" value={title} checked={true}/>
+        <input type="radio" name="radio" id="radio_icon" value={title} defaultChecked={true}/>
       :
         <input type="radio" name="radio" id="radio_icon" value={title} />
       }
