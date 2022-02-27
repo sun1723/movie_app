84c3afb1ae44624206816d9194c3a3124570f70a
sun <sunpeiting07@gmail.com>
2022-02-28 08:29:21 +1030

    feat: enable user visual score list in movie detail modal

---
+++ src/components/ScoreBox.js
@@ -0,0 +1,12 @@
+import '../utils/score_box.scss'
+import { ContentDetail } from './ContentDetail'
+
+export const ScoreBox = ({rate, noBorder}) => {
+  console.log(noBorder)
+  return (
+    <div className={noBorder ? 'score-box no_border' : 'score-box'}>
+      <ContentDetail title="value" content={rate.Value} noTitle={true}/>
+      <ContentDetail title="source" content={rate.Source} noTitle={true}/>
+    </div>
+  )
+}
