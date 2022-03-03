ec678c24f9cd693c2ebbfd6849eae9efc9978c29
sun <sunpeiting07@gmail.com>
2022-03-03 13:50:11 +1030

    fix: MA-41-score list not central align

--- src/components/ScoreBox.js
+++ src/components/ScoreBox.js
@@ -2,7 +2,6 @@ import '../utils/score_box.scss'
 import { ContentDetail } from './ContentDetail'
 
 export const ScoreBox = ({rate, noBorder}) => {
-  console.log(noBorder)
   return (
     <div className={noBorder ? 'score-box no_border' : 'score-box'}>
       <ContentDetail title="value" content={rate.Value} noTitle={true}/>
